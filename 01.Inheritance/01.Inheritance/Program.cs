7923781d11f86da4593a4cf43ac0e7161e93ef30
Joshhortt <joshhortt@yahoo.com>
2021-03-09 15:46:31 +0000

    Add comments to Homework 01.

--- 01.Inheritance/01.Inheritance/Program.cs
+++ 01.Inheritance/01.Inheritance/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// 01 - Homework - Inheritance
+// Implement a four classes: car, vehicle, bus and boat. Identify what inheritance should happen, if any, and wire it up.
+
+// Note that a vehicle can be anything from a motorcycle to a car or bus even a boat.
+// We all know that vehicles share too many specification and functionalities among them.
+// So, how about having one class that can hold implements
+// all these shared areas instead of repeating them in each different class?
+// This is when inheritance gets involved and it's very helpful in Development.
+
+//A base class is the class that act as a parent class of other classes. It’s a class that gives both it's data and behavior
+//to derived class, such as the vehicle class giving its members to the Car class for example.
+//The base class doesn’t inherit from any other class. 
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
