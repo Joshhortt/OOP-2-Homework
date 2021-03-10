7c33749367488627e5b8293f2c73abdb1684c8f3
Joshhortt <joshhortt@yahoo.com>
2021-03-10 16:07:53 +0000

    Fc. Add Homework Mini-Project about Inheritance and Interfaces.

---
+++ 03.MiniProjectSimilar/AlternativeInvestmentsApp/AlternativeInvestments/Program.cs
@@ -0,0 +1,17 @@
+﻿// HOMEWORK - 03 - Mini Project Similar (Inheritance & Interfaces)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternativeInvestments
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadLine();
+		}
+	}
+}
