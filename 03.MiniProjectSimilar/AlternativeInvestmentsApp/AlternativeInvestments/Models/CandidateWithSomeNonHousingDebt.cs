da73d4331b5d12427760f5f4a50a760695e22efe
Joshhortt <joshhortt@yahoo.com>
2021-03-10 18:58:33 +0000

    Removed commented code.

--- 03.MiniProjectSimilar/AlternativeInvestmentsApp/AlternativeInvestments/Models/CandidateWithSomeNonHousingDebt.cs
+++ 03.MiniProjectSimilar/AlternativeInvestmentsApp/AlternativeInvestments/Models/CandidateWithSomeNonHousingDebt.cs
@@ -9,17 +9,6 @@ namespace AlternativeInvestments.Models
 {
 	public class CandidateWithSomeNonHousingDebt : RentToOwnHomesModel, ILeaseReturn
 	{
-		//public void OptionBuy()
-		//{
-		//	QuantityAvailable -= 1;
-		//	Console.WriteLine("You are qualified to lease the property and you can buy it, before the end of the lease.");
-		//}
-
-		//public void OptionReturn()
-		//      {
-		//          QuantityAvailable += 1;
-		//	Console.WriteLine("You are qualified not to buy the property at the end of the lease, the option simply expires.");
-		//}
 		public void Return()
 		{
 			QuantityAvailable += 1;
