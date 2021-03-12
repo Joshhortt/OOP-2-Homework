8b2fa4c3eaa63ff0e58c1be6906ec86cde361358
Joshhortt <joshhortt@yahoo.com>
2021-03-12 18:33:18 +0000

    Add Homework 06 about Method overriding.

---
+++ 06.MethodOverride/MethodOverrideHomework/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace MethodOverrideHomework
+{
+	public class Person
+        {
+		    public int Id { get; set; }
+		    public string FirstName { get; set; }
+            public string LastName { get; set; }
+           
+            public override string ToString()
+            {
+                return $"Your Id is: " + this.Id + " Your first name is: " + this.FirstName + " Your last name is: " + this.LastName;
+            }
+        }
+    }
---
+++ 06.MethodOverride/MethodOverrideHomework/Program.cs
@@ -0,0 +1,22 @@
+﻿// 06 - HOMEWORK - Method Overriding
+// Create a Person class and override the ToString method in it. Make it return the user personal details.
+// First name, Last name, etc...
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverrideHomework
+{
+	class Program
+        {
+            static void Main(string[] args)
+            {
+                Person control = new Person() {Id = 99, FirstName = "Josh", LastName = "Hortt" };
+
+                Console.WriteLine(control.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
