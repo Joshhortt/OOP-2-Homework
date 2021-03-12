f7e2f993305f7fd60d9dff8f334a3c3c06dfe7cc
Joshhortt <joshhortt@yahoo.com>
2021-03-12 13:22:15 +0000

    Add Homework 05 part 1, about Abstract classes.

---
+++ 05.AbstractClasses/AbstractClasses/IPhoneX.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace AbstractClass
+{
+	class IPhoneX : Phone
+	{
+		public override void Model()
+		{
+			Console.WriteLine("Model: The model of this Phone is iPhone-X");
+		}
+
+		public void LaunchDate()
+		{
+			Console.WriteLine("Launch Date: This Phone was launched on November 3, 2017");
+		}
+	}
+}
---
+++ 05.AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,34 @@
+﻿// 05 - HOMEWORK - Abstract Classes part 1
+// Create Console Application with an Abstract Class.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AbstractClass
+{
+	abstract class Phone 
+    {
+        public void Call()
+        {
+            Console.WriteLine("Call Method: This method provides Calling features");
+        }
+        public abstract void Model();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPhoneX iphone = new IPhoneX();
+
+            iphone.Call();
+            iphone.Model();
+            iphone.LaunchDate();
+
+            Console.ReadLine();
+        }
+    }
+}
