dcd19821c19916b488fb1301ad7bc75f57b3b71f
Joshhortt <joshhortt@yahoo.com>
2021-03-15 20:00:14 +0000

    Add Homework 09 Method Overloading 'New Employee'. Add 'Employee Model' class with '3 Constructors'. Instantiate to console.

---
+++ 09.MethodOverloadNewEmployee/MethodOverloadHomework/Models/NewEmployee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MethodOverloadHomework
+{
+    partial class Program
+    {
+        public class NewEmployee
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string ID{ get; set; }
+            public string Department { get; set; }
+            public string Team { get; set; }
+
+            public NewEmployee() : this("Josh", "Default lastName")
+            {
+                Console.WriteLine("Reserving a new record for the new employee.");
+            }
+            public NewEmployee(string firstName) : this(firstName, "Default lastName")
+            {
+                this.FirstName = firstName;
+                Console.WriteLine("Creating a record for the new employee, with firstName!");
+            }
+            public NewEmployee(string firstName, string lastName) : this(firstName, lastName, "JH04", "Development Department", "The C# team")
+            {
+                this.LastName = lastName;
+                this.FirstName = firstName;
+                Console.WriteLine("Creating a record for the new employee, with firstName and lastName!");
+            }
+            public NewEmployee(string firstName, string lastName, string id, string department, string team)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.ID = id;
+                this.Department = department;
+                this.Team = team;
+                Console.WriteLine("Creating a record for the new employee, with firstName, lastName, id, department & team!");
+            }
+        }
+    }
+}
---
+++ 09.MethodOverloadNewEmployee/MethodOverloadHomework/Program.cs
@@ -0,0 +1,34 @@
+﻿// 09 - HOMEWORK - Method Overloading
+// Create a Employee model class and create three different constructors, that take in different amounts of data.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadHomework
+{
+	partial class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********** First Constructor! **********");
+            NewEmployee first = new NewEmployee();
+            Console.WriteLine(first.FirstName);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("********** Second Constructor! **********");
+            NewEmployee second = new NewEmployee();
+            Console.WriteLine(second.Department);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("********** Third Constructor! **********");
+            NewEmployee third = new NewEmployee();
+            Console.WriteLine(third.Team);
+
+            Console.ReadLine(); ;
+        }
+    }
+}
