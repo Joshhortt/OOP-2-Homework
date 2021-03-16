6057a473972b437b8a4a8d412c2b0279e158736e
Joshhortt <joshhortt@yahoo.com>
2021-03-16 17:52:07 +0000

    Moved classes to class files.

---
+++ 10.ExtensionMethodPerson/ExtensionMethodHomework/MorePersons.cs
@@ -0,0 +1,41 @@
+﻿// 10 - HOMEWORK - Extension Methods
+// Create a chain using extension methods and Fluent Design pattern.
+
+namespace ExtensionMethodHomework
+{
+	public static class MorePersons
+    {
+        public static Person OfSmallSize(this Person person)
+        {
+            person.Weigth = 50;
+            person.Height = 150;
+            person.HasAge = 18;
+            return person;
+        }
+
+        public static Person OfMediumSize(this Person person)
+        {
+            person.Weigth = 75;
+            person.Height = 170;
+            person.HasAge = 18;
+            return person;
+        }
+
+        public static Person OfLargeSize(this Person person)
+        {
+            person.Weigth = 100;
+            person.Height = 190;
+            person.HasAge = 18;
+            return person;
+        }
+
+        public static Person OfSize(this Person person, int weigth, int height, int hasAge)
+        {
+            person.Weigth = weigth;
+            person.Height = height;
+            person.HasAge = hasAge;
+
+            return person;
+        }
+    }
+}
---
+++ 10.ExtensionMethodPerson/ExtensionMethodHomework/Person.cs
@@ -0,0 +1,48 @@
+﻿// 10 - HOMEWORK - Extension Methods
+// Create a chain using extension methods and Fluent Design pattern.
+
+namespace ExtensionMethodHomework
+{
+	public class Person
+    {
+		public string Name { get; set; }
+		public int Weigth { get; set; }
+		public int Height { get; set; }
+		public int HasAge { get; set; }
+
+	
+        private Person()
+        {
+        }
+
+        public static Person Create()
+        {
+            return new Person() { Name = "My Person", Weigth = 75, Height = 170, HasAge = 18 };
+            
+        }
+
+        public Person WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+
+        public Person OfSize(int weigth, int height)
+        {
+            Weigth = weigth;
+            Height = height;
+            return this;
+        }
+
+        public Person WithAge()
+        {
+            HasAge = 18;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("\nName: {0}\nWeigth: {1}\nHeight: {2}\nAge: {3}", Name, Weigth, Height, HasAge);
+        }
+    }
+}
