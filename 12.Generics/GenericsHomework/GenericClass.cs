c657018ba945f244ee306f3ea2ae876635835f69
Joshhortt <joshhortt@yahoo.com>
2021-03-17 13:00:41 +0000

    Moved class to class file. Update comments.

---
+++ 12.Generics/GenericsHomework/GenericClass.cs
@@ -0,0 +1,31 @@
+﻿// 12 - HOMEWORK - Generics
+
+// Create a generic class with a generic constructor, generic member variable, generic property, and a generic method,
+//that takes in an item and calls ToString() method and prints that value to the console.
+
+using System;
+
+namespace GenericsHomework
+{
+	// GenericClass is a Generic Class
+	public class GenericClass<T>
+    {
+        //Generic variable. The data type is generic
+        private T GenericMemberVariable;
+
+        //Generic Constructor. Constructor accepts one parameter of Generic type
+        public GenericClass(T value)
+        {
+            GenericMemberVariable = value;
+        }
+        //Generic Method. Method accepts one Generic type Parameter. Method return type also Generic
+        public T GenericMethod(T genericParameter)
+        {
+            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);
+            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), GenericMemberVariable);
+            return GenericMemberVariable;
+        }
+        //GenericProperty is a generic property
+        public T GenericProperty { get; set; }
+    }
+}
--- 12.Generics/GenericsHomework/Program.cs
+++ 12.Generics/GenericsHomework/Program.cs
@@ -1,5 +1,8 @@
 ﻿// 12 - HOMEWORK - Generics
-// Create a generic method that takes in an item and calls ToString() method and prints that value to the console.
+
+// Create a generic class with a generic constructor, generic member variable, generic property, and a generic method,
+//that takes in an item and calls ToString() method and prints that value to the console.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,25 +31,4 @@ namespace GenericsHomework
             Console.ReadLine();
 		}
 	}
-    // GenericClass is a Generic Class
-    public class GenericClass<T>
-    {
-        //Generic variable. The data type is generic
-        private T GenericMemberVariable;
-
-        //Generic Constructor. Constructor accepts one parameter of Generic type
-        public GenericClass(T value)
-        {
-            GenericMemberVariable = value;
-        }
-        //Generic Method. Method accepts one Generic type Parameter. Method return type also Generic
-        public T GenericMethod(T genericParameter)
-        {
-            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);
-            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), GenericMemberVariable);
-            return GenericMemberVariable;
-        }
-        //GenericProperty is a generic property
-        public T GenericProperty { get; set; }
-    }
 }
