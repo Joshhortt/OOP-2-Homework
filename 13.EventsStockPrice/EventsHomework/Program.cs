a1ef16a09a30f4a0e0f7ffa43a78ec57b3218cb2
Joshhortt <joshhortt@yahoo.com>
2021-03-17 17:23:54 +0000

    Add project 13 homework 'EventsStockPrice' about 'Events, delegates and listeners'.

---
+++ 13.EventsStockPrice/EventsHomework/Program.cs
@@ -0,0 +1,33 @@
+﻿// 13 - HOMEWORK - Events
+// Create an event in a class then consume that class. Attach a listener to the event and have it write to the console.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsHomework
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			// The predefined non-generic EventHandler delegate can be used when an event doesn't carry extra information:
+
+			Stock stock = new Stock("Apple")
+            // stock.Price = 28.46M;   -- >  simplified version below
+            {
+				Price = 28.46M
+			};
+
+			// Register with the PriceChanged event
+			stock.PriceChanged += stock_PriceChanged;
+            stock.Price = 29.92M;
+
+            void stock_PriceChanged(object sender, EventArgs e)
+            {
+                Console.WriteLine("New Stock price is: " + ((Stock)sender).Price);
+            }
+        }
+    }
+}
---
+++ 13.EventsStockPrice/EventsHomework/Stock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EventsHomework
+{
+	public class Stock
+    {
+		private string symbol;
+		private decimal price;
+
+        public Stock(string symbol) { this.symbol = symbol; }
+
+        public event EventHandler PriceChanged;
+
+        protected virtual void OnPriceChanged(EventArgs e)
+        {
+            PriceChanged?.Invoke(this, e);
+        }
+
+		public decimal Price
+		{
+			get { return price; }
+			set
+			{
+				if (price == value) return;
+				price = value;
+				OnPriceChanged(EventArgs.Empty);
+			}
+		}
+
+		public Stock(decimal price)
+		{
+			Price = price;
+		}
+	}
+}
